04e5e284a2ae0021984bfb88a53f966585bd3d09
bernborgess <bernborgess@outlook.com>
2024-03-17 23:55:47 +0000

    Simple OK response

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ module Main (main) where
 import Control.Monad (forever)
 import qualified Data.ByteString.Char8 as BC
 import Network.Socket
+import Network.Socket.ByteString
 import System.IO (BufferMode (..), hSetBuffering, stdout)
 
 main :: IO ()
@@ -17,20 +18,22 @@ main = do
     -- Uncomment this block to pass first stage
     let host = "127.0.0.1"
         port = "4221"
-    --
+
     BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
-    
+
     -- Get address information for the given host and port
     addrInfo <- getAddrInfo Nothing (Just host) (Just port)
-    
+
     serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
     bind serverSocket $ addrAddress $ head addrInfo
     listen serverSocket 5
-    
+
     -- Accept connections and handle them forever
     forever $ do
         (clientSocket, clientAddr) <- accept serverSocket
         BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
         -- Handle the clientSocket as needed...
-    
+
+        _ <- send clientSocket "HTTP/1.1 200 OK\r\n\r\n"
+
         close clientSocket
